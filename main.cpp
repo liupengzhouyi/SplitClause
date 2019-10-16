c6ac8bad09b1dad6d61609612e5c221d900adbd9
liupeng <liupeng.0@outlook.com>
2019-10-16 20:45:05 +0800

    实现C++11环境下的计算频率B.读取文件

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <codecvt>
 #include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
@@ -81,36 +82,53 @@ public:
 
 int main(int argc, char *argv[])
 {
-    string str = "这是一句临时字典长度太大，需要合并到总临时字典长度太大，需要合并到总字这是一句话，话，这是一这临时字临时字典长度太大，需要合并到总典长度太大，需要合并到总是一话，句话，这是一句话，这是一话，这是一这是一话，句话，这是一句句话，这是一句话，";
-
-    // string to wstring
-    wstring wString = s2ws(str);
-
-    // 临时变量：记录临时字典的长度
+    // "3", "4", "5", "6", "7", "8"
+    string strs[10] = {"1", "2"};
+    ifstream ifs;
     int longth = 0;
 
-    Node nodeList[1000];
+    Node nodeList[100000];
+    string s;
 
-    // 核心代码
-    for (int i=0; i<wString.length()-1; i++) {
-        // 获取词组
-        //wstring newStr = wstring(1,wchar_t(wString[i])) + wstring(1,wchar_t(wString[i+1]));
-        string newStr = to_string((int)wString[i]) + "-" + to_string((int)wString[i+1]);
-        cout << newStr << endl;
-        bool key = false;
-        for (int i=0; i<longth+1; i++){
-            if (nodeList[i].getStr() == newStr) {
-                nodeList[i].addNum();
-                key = true;
-                break;
+    ifs.open("/Users/liupeng/CLionProjects/SplitClause/papers/papers/1 (1).txt");   			//将文件流对象与文件关联起来，如果已经关联则调用失败
+    assert(ifs.is_open());   	//若失败,则输出错误消息,并终止程序运行
+
+    while(getline(ifs,s))		//行分隔符可以显示指定，比如按照分号分隔getline(infile,s,';')
+    {
+        cout << longth << endl;
+        cout<<s<<endl;
+        string str1 = "这是一句临时字典长度太大，需要合并到总临时字典长度太大，需要合并到总字这是一句话，话，这是一这临时字临时字典长度太大，需要合并到总典长度太大，需要合并到总是一话，句话，这是一句话，这是一话，这是一这是一话，句话，这是一句句话，这是一句话，";
+        str1 = s;
+
+        // string to wstring
+        wstring wString = s2ws(str1);
+
+        // 临时变量：记录临时字典的长度
+
+
+        // 核心代码
+        for (int i=0; i<wString.length()-1; i++) {
+            // 获取词组
+            //wstring newStr = wstring(1,wchar_t(wString[i])) + wstring(1,wchar_t(wString[i+1]));
+            string newStr = to_string((int)wString[i]) + "-" + to_string((int)wString[i+1]);
+            // cout << newStr << endl;
+            bool key = false;
+            for (int i=0; i<longth+1; i++){
+                if (nodeList[i].getStr() == newStr) {
+                    nodeList[i].addNum();
+                    key = true;
+                    break;
+                }
+            }
+            if (!key) {
+                nodeList[longth].setStr(newStr);
+                nodeList[longth].setNumber(1);
+                longth ++;
             }
         }
-        if (!key) {
-            nodeList[longth].setStr(newStr);
-            nodeList[longth].setNumber(1);
-            longth ++;
-        }
     }
+    ifs.close();
+    //关闭文件输入流
 
     for (int i=0;i<longth;i++) {
         string str = nodeList[i].getStr();
@@ -128,13 +146,17 @@ int main(int argc, char *argv[])
                 sss = sss + str[i];
             }
         }
-        cout << sss << " - " << ss << endl;
+        // cout << sss << " - " << ss << endl;
         cout << nodeList[i].getStr() << "-" << nodeList[i].getNumber() << endl;
     }
 
     return 0;
 }
 
+// 编码
 
+// 统计
+
+// 解码
 
 
